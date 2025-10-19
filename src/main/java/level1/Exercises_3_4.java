a8f58c3591dac6f8f48b1945796d3a0d5de80272
Esther <githubEsther@gmail.com>
2025-10-19 12:17:50 +0200

    Feat: third exercise level 1. Showing months with lambda for each

---
+++ src/main/java/level1/Exercises_3_4.java
@@ -0,0 +1,30 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class Exercises_3_4 {
+    private static List<String> monthsList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        addingMonths();
+        exercise1();
+    }
+
+    private static void addingMonths() {
+        Calendar calendar = Calendar.getInstance();
+
+        for (int i = 0; i < 11; i++) {
+            calendar.set(2025, i, 1);
+            String monthName = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.UK);
+            monthsList.add(monthName);
+        }
+    }
+
+    private static void exercise1() {
+        monthsList.stream().forEach(System.out::println);
+
+    }
+}
