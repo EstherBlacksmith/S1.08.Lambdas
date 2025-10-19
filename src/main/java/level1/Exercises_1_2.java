7498efd95578c5ad48f6f92b06cacaa55104adcb
Esther <githubEsther@gmail.com>
2025-10-19 12:05:11 +0200

    Feat: second exercise level 1. Adding second filter

--- src/main/java/level1/Exercises_1_2.java
+++ src/main/java/level1/Exercises_1_2.java
@@ -2,9 +2,12 @@ package level1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Exercises_1_2 {
+    final static int FIVE = 5;
+
     public static void main(String[] args) {
         List<String> StringListForManipulation = new ArrayList<>();
 
@@ -20,12 +23,27 @@ public class Exercises_1_2 {
         for (String onlyO : onlyOList) {
             System.out.println(onlyO);
         }
+        List<String> onlyOListAndFive = exercise2(StringListForManipulation);
+        for (String onlyOAnd5 : onlyOListAndFive) {
+            System.out.println(onlyOAnd5);
+        }
     }
 
     private static List<String> exercise1(List<String> StringListForManipulation) {
         List<String> StringListOnlyO = null;
         StringListOnlyO = StringListForManipulation.stream()
                 .filter(stringList -> stringList.contains("o")).collect(Collectors.toList());
+        return StringListOnlyO;
+
+    }
+
+    private static List<String> exercise2(List<String> StringListForManipulation) {
+        List<String> StringListOnlyO = null;
+        Predicate <String> OnlyOes = stringList -> stringList.contains("o");
+        Predicate <String> minLength5 = stringList -> stringList.length() >= FIVE;
+
+        StringListOnlyO = StringListForManipulation.stream()
+                .filter(OnlyOes.and(minLength5)).collect(Collectors.toList());
         return StringListOnlyO;
 
     }
