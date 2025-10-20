49dffb03339f4072729d6d837fc7107542e18c95
Esther <githubesther@gmail.com>
2025-10-20 12:30:48 +0200

    Refactor: refactoring code and creating a static method for return the lambda body

--- src/main/java/level1/Exercise_8/Main_8.java
+++ src/main/java/level1/Exercise_8/Main_8.java
@@ -1,13 +1,19 @@
 package level1.Exercise_8;
 
+
 public class Main_8 {
     public static void main(String[] args) {
         String str = "AEIOU";
 
-        InsideOut reversedString = (inputString) -> inputString.chars()
-                .mapToObj(c -> (char) c)
-                .reduce("", (s, c) -> c + s, (s1, s2) -> s2 + s1);
+        InsideOut reversedString = lambdaBody();
 
         System.out.println(reversedString.reverse(str));
     }
+
+    static InsideOut lambdaBody() {
+        return (inputString) -> inputString.chars()
+                .mapToObj(c -> (char) c)
+                .reduce("", (s, c) -> c + s, (s1, s2) -> s2 + s1);
+    }
+
 }
